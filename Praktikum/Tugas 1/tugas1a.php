b48bc5d33c0fc86f33c8ebd9031b2a2b2fa3304e
sienjip <sienjip01@gmail.com>
2021-03-09 23:14:20 +0700

    first commit

---
+++ Praktikum/Tugas 1/tugas1a.php
@@ -0,0 +1,12 @@
+<!-- 
+    Nama	: Nazhif Hafidz Fauzan
+    NRP 	: 203040018
+    Shift 	: Rabu pukul 09:00-10:00 WIB
+-->
+
+<?php for ($x=1; $x<=3; $x++){
+    for ($z=1; $z<=3; $z++){
+        echo "Ini perulangan ke ($x,$z)<br>";
+    }
+}
+?>
